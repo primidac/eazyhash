7acd5062de9d1fad5591a8df0bfb0a12e3876e36
primidac <primidac@gmail.com>
2020-07-14 21:59:12 +0100

    60% complete

--- ezhash.py
+++ ezhash.py
@@ -1,54 +1,50 @@
-# hash any string easily with ezhash
+# hash any string and get it copied directly to your clipboard
 
-# importing all modules required by the program
-from hashlib import *
+# importing all modules required for the program to run
 from colorama import Fore, Style
 from tqdm import tqdm
-import pyfiglet
+from time import sleep
+from hashlib import *
+import os
 import pyperclip
-
-
-# hashing function
-def ezhashed(value):
-    hashed_algo = {'shake_128': shake_128, 'sha256': sha256, 'sha3_256': sha3_256, 'sha3_224': sha3_224, 'sha1': sha1, 'sha224': sha224, 'sha384': sha384 , 'sha512': sha512, 'sha3_384': sha3_384, 'blake2b': blake2b, 'blake2s': blake2s , 'shake_256': shake_256, 'sha3_512': sha3_512, 'md5': md5}
-    name = 'primidac'
-    if value in hashed_algo.keys():
-        value(name.encode()).hexdigest()
+import pyfiglet
 
 # main program begins here
-# making our parser
-ez_parser = argparse.ArgumentParser(prog="ezhash",
-    description="EzHash is a simple tool to hash any string from your command line and get it copied directly to your clipboard")
-
-# making our command for our programs
-ez_parser.add_argument(
-    "-v",
-    "--version",
-    action='store_true',
-    help='Displays the current version of EzHash you are running'
-)
-
-ez_parser.add_argument(
-    "-hs",
-    "--hash",
-    "-ha",
-    "--hash_algo",
-    action='store_true',
-    # type='int',
-    help='Returns the hash of any value'
-)
-
-# creating our parser object
-args = ez_parser.parse_args()
-
-# bringing our program to life
-if args.version:
-    print('Running V1.0.4')
+header =  Fore.GREEN + pyfiglet.figlet_format('EzHash', font='roman')
+print(header)
+print(Style.RESET_ALL)
+print("...Ezhash values")
 
 
-elif args:
-    # program header
-    header = pyfiglet.figlet_format('EzHash' , font='roman')
-    print(Fore.GREEN + header)
-    print(Style.RESET_ALL)
-    os.system('python3 ezhash.py -h')
+# command list
+print('*' * 20)
+print(' [*]Hash\n', 
+      '[*]Version\n',
+      '[*]Exit'
+      )
+print('*' * 20)
+
+command = ['Hash', 'version', 'exit']
+hash_algo = {'shake_128': shake_128, 'sha3_256': sha3_256, 'shake_256': shake_256, 'sha256': sha256, 'md5': md5, 'blake2s': blake2s, 'blake2b': blake2b, 'sha3_384': sha3_384, 'sha512' : sha512, 'sha3_224': sha3_224, 'sha224': sha224, 'sha3_512':sha3_512, 'sha384': sha384, 'sha1': sha1}
+# prompt
+while True:
+    try:
+        prompt = input('=>')
+        if prompt == command[0]:
+        	myhash = input('Hash Algo')
+        	string = input('String:')
+        	if myhash in hash_algo.keys():
+        		hashed = hash_algo[myhash](string.encode()).hexdigest()
+        	else:
+        		print("Algorithm do not exist or not supported yet")
+        		print(Fore.GREEN)
+        		for x in tqdm(range(int(100)), ascii=True, desc='Hashing'):
+        			# pass
+        			sleep(0.1)
+        		pyperclip.copy(hashed)
+        		print('hash copy to clipboard')
+        		print(Style.RESET_ALL)
+
+    except Exception as e:
+    	raise e
+
